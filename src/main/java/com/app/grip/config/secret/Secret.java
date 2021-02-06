a26bfa2a9e329dd0be7eba6b4c38b6607fb9a7b9
binna <every5116@naver.com>
2021-02-07 02:03:16 +0900

    gitignore 적용

--- src/main/java/com/app/grip/config/secret/Secret.java
+++
@@ -1,5 +0,0 @@
-package com.app.grip.config.secret;
-
-public class Secret {
-    public static String USER_INFO_PASSWORD_KEY = "o9pqYVC9-F8_.PEzEiw!L9F6.AYj9jcfVJ*_i.ifXYnyE68kix@Q2dL6rw*bV-rpdZYwcqZG-jPF-fw3CiJyKsfZ778ks-*jnZn";
-}
