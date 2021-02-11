c25b75b12daf6bf3a50ae5f74d96ab0a0390508e
ahnseongeun <ast3138@naver.com>
2021-02-11 16:52:39 +0900

    영상 외부경로 주입 테스트

---
+++ src/main/java/com/app/grip/config/WebConfig.java
@@ -0,0 +1,21 @@
+package com.app.grip.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/*
+외부 경로 매핑하기
+
+ */
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+        @Override
+        public void addResourceHandlers(ResourceHandlerRegistry registry) {
+            registry.addResourceHandler("/api/videos/**")
+                    //.addResourceLocations("file:///home/ubuntu/")
+                    .addResourceLocations("file:/home/ubuntu/");
+        }
+
+}
