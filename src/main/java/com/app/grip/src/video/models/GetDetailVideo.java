960ea2d5579b9306406aa022f54dc508be1268a3
ahnseongeun <ast3138@naver.com>
2021-02-17 18:42:33 +0900

    영상 시작위치

--- src/main/java/com/app/grip/src/video/models/GetDetailVideo.java
+++ src/main/java/com/app/grip/src/video/models/GetDetailVideo.java
@@ -13,6 +13,7 @@ public class GetDetailVideo {
     private final String hostName;
     private final String hostImageURL;
     private final String liveCheck;
+    private final Integer startTime;
     private final Integer watchUserCount;
     private final Integer videoLikeCount;
     private final Long storeId;
