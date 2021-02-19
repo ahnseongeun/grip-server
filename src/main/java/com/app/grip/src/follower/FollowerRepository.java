a70e5cb364947d346fab746b35b182337b6ee885
binna <every5116@naver.com>
2021-02-19 14:43:55 +0900

    팔로워 팔로잉 제외

--- src/main/java/com/app/grip/src/follower/FollowerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.app.grip.src.follower;
-
-import com.app.grip.src.follower.models.Follower;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface FollowerRepository extends CrudRepository<Follower, Long> {
-}
--- src/main/java/com/app/grip/src/following/FollowingRepository.java
+++
@@ -1,7 +0,0 @@
-package com.app.grip.src.following;
-
-import com.app.grip.src.following.models.Following;
-import org.springframework.data.repository.CrudRepository;
-
-public interface FollowingRepository extends CrudRepository<Following, Long> {
-}
